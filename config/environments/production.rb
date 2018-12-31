9318d3ae5793e00465ecab3b36a1bfce6a01761d
donatienrolland <donatien@rollandmail.com>
2018-12-31 10:17:14 +0700

    chang smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method     = :sendgrid
-  config.action_mailer.sendgrid_settings   = { api_key: ENV['SENDGRID_API_KEY'] }
+  # config.action_mailer.sendgrid_settings   = { api_key: ENV['SENDGRID_API_KEY'] }
   config.action_mailer.default_url_options = { host: "commonpointv2.herokuapp.com" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
--- config/environments/smtp.rb
+++ config/environments/smtp.rb
@@ -1,11 +1,12 @@
 
 
 ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: "smtp.sendgrid.net",
   port: 587,
-  domain: 'gmail.com',
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_APP_PASSWORD'],
+  domain: 'http://commonpointv2.herokuapp.com',
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
   authentication: :login,
   enable_starttls_auto: true
 }
+
